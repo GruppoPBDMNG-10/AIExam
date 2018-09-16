5309d4175b236037fa41c97ec8f51f3a03093732
Gaetano Ziri <gaetano_ziri@live.it>
2018-09-16 23:23:11 +0200

    taxy-predict

--- experimentation/clustering/clustering.py
+++ experimentation/clustering/clustering.py
@@ -1,115 +1,56 @@
 import numpy as np
 import geopy.distance
-from scipy.cluster.vq import kmeans2
 import json
 from sklearn.cluster import KMeans, MiniBatchKMeans
-import matplotlib.pyplot as plt
-from scipy.spatial import distance
 
 
 class Result(object):
-    clusters = [];
-    coordinates = [];
-    labels = [];
-    distance = [];
-    dict = {};
-    mean = 0;
-    median = 0;
-    variance = 0;
+    mean = 0
+    median = 0
+    variance = 0
+    model = object
 
     # The class "constructor" - It's actually an initializer
-    def __init__(self, clusters, coordinates, labels, distance, dict, mean, median, variance):
-        self.clusters = clusters;
-        self.coordinates = coordinates;
-        self.labels = labels;
-        self.dict = dict;
-        self.distance = distance;
-        self.mean = mean;
-        self.median = median;
-        self.variance = variance;
+    def __init__(self, mean, median, variance, model):
+        self.mean = mean
+        self.median = median
+        self.variance = variance
+        self.model = model
 
-    def make_result(clusters, coordinates, labels, distance, dict, mean, median, variance):
-        result = Result(clusters, coordinates, labels, dict, distance, mean, median, variance);
-        return result;
+    def make_result(self, mean, median, variance, model):
+        result = Result(self, mean, median, variance, model)
+        return result
 
     def __repr__(self):
-        #return jsonpickle.encode(self);
-        return json.dumps(self.__dict__);
-
-
-def make_clustering(coordinates, clusters=int, iter=int):
-    centroids, labels = kmeans2(coordinates, 200, iter=iter, minit="points");
-
-    #Calculate distance between centroids and points
-    dist = [None] * len(coordinates);
-    dict = {};
-    key = [];
-    value = [];
-    for i in range(len(coordinates)):
-        key = coordinates[i];
-        value = centroids[labels[i]];
-        dict[','.join(map(str, key))] = value;
-        dist[i] = geopy.distance.vincenty((key[0], key[1]), (value[0], value[1])).meters;
-
-    # Calculate mean
-    mean = np.mean(dist, dtype=np.float64);
-
-    # Calculate median
-    median = np.median(dist);
-
-    # Calculate variance
-    variance = np.var(dist, dtype=np.float64);
-
-    return Result(centroids.tolist(), coordinates, labels.tolist(), dict, dist, mean.tolist(), median.tolist(), variance.tolist());
+        return json.dumps(self.__dict__)
 
 
 def make_clustering_2(coordinates, clusters=int):
-    #kmeans_model = KMeans(n_clusters=clusters, random_state=1).fit(coordinates);
-    kmeans_model = MiniBatchKMeans(n_clusters=clusters, random_state=1, init_size=3*clusters).fit(coordinates);
+    # model = KMeans(n_clusters=clusters, random_state=1).fit(coordinates)
+    model = MiniBatchKMeans(n_clusters=clusters, random_state=1, init_size=3*clusters).fit(coordinates)
 
     # These are our fitted labels for clusters -- the first cluster has label 0, and the second has label 1.
-    labels = kmeans_model.labels_;
-
-    centroids = kmeans_model.cluster_centers_;
+    labels = model.labels_
+    centroids = model.cluster_centers_
 
     # Calculate distance between centroids and points
-    dist = [None] * len(coordinates);
-    dict = {};
-    key = [];
-    value = [];
+    dist = [None] * len(coordinates)
     for i in range(len(coordinates)):
-        key = coordinates[i];
-        value = centroids[labels[i]];
-        dict[','.join(map(str, key))] = value.tolist();
-        dist[i] = geopy.distance.vincenty((key[0], key[1]), (value[0], value[1])).meters;
+        key = coordinates[i]
+        value = centroids[labels[i]]
+        dist[i] = geopy.distance.vincenty((key[0], key[1]), (value[0], value[1])).meters
 
     # Calculate mean
-    mean = np.mean(dist, dtype=np.float64);
+    mean = np.mean(dist, dtype=np.float64)
 
     # Calculate median
-    median = np.median(dist);
+    median = np.median(dist)
 
     # Calculate variance
-    variance = np.var(dist, dtype=np.float64);
+    variance = np.var(dist, dtype=np.float64)
 
-    return Result(centroids.tolist(), [], labels.tolist(), dict, dist, mean, median, variance);
+    return Result(mean, median, variance, model)
 
 
-"""result = make_clustering(coordinates, 200, 200);
-print(result);
-
-result = make_clustering_2(coordinates, 200);
-print(result);
-
-sum_of_squared_distances = [];
-K = range(1, 500);
-
-for k in K:
-    result = make_clustering_2(coordinates, k);
-    sum_of_squared_distances.append(result.mean);
-
-plt.plot(K, sum_of_squared_distances, 'bx-');
-plt.xlabel('k');
-plt.ylabel('Sum_of_squared_distances');
-plt.title('Elbow Method For Optimal k');
-plt.show();"""
+def predict(model, coordinates):
+    return KMeans.predict(model, [(float(coordinates[0]), float(coordinates[1]))])[0]
--- experimentation/taxi-repository/preprocess.py
+++ experimentation/taxi-repository/preprocess.py
@@ -1,3 +1,4 @@
+import re
 import pandas as pd
 import itertools
 from common import common
@@ -5,82 +6,94 @@ from clustering import clustering
 import geopy.distance
 import numpy as np
 from sqlalchemy import create_engine
+from sklearn.externals import joblib
 
 
-def polyline_to_coordinates(polyline):
+def polyline_to_coordinates(polyline, regex):
     """It transforms the polyline column in coordinates array"""
-    coordinates = [];
-    list = polyline.replace("[", "").replace("]", "").split(",");
-    if len(list) > 1 & len(list) % 2 == 0:
-        for i in range(0, len(list) - 1, 2):
-            coordinates.append((float(list[i]),float(list[i+1])));
-    return coordinates;
+    coordinates_array = []
+    matches = re.finditer(regex, polyline)
+    # matches = re.finditer(r'\[(-?[0-9]{1,2}\.[0-9]+),\s(-?[0-9]{1,2}\.[0-9]+)\]', polyline)
+    for match in matches:
+        coordinates_array.append((float(match.group(1)), float(match.group(2))))
+
+    return coordinates_array
 
 
-def drop_columns(df=pd.DataFrame, columsName=list):
-    """Drop specified colums from the required dataframe"""
-    return df.drop(columsName, axis=1);
+def drop_columns(data_frame=pd.DataFrame, columns_name=list):
+    """Drop specified columns from the required data frame"""
+    return data_frame.drop(columns_name, axis=1)
 
-#csv_database = create_engine('sqlite:///taxi.db');
-result_folder = 'result/';
-file = 'dataset/taxi.zip';
-result_csv = result_folder + 'taxi_cleaned.csv';
-result_coordinates = result_folder + 'coordinates.csv';
 
-columns_to_remove = ["CALL_TYPE","ORIGIN_CALL","ORIGIN_STAND","DAY_TYPE","MISSING_DATA"];
+# csv_database = create_engine('sqlite:///taxi.db')
+result_folder = 'result/'
+file = 'dataset/taxi_small.zip'
+result_csv = result_folder + 'taxi_cleaned.csv'
+result_coordinates = result_folder + 'coordinates.csv'
+result_model = result_folder + 'model.pkl'
+columns_to_remove = ["CALL_TYPE", "ORIGIN_CALL", "ORIGIN_STAND", "DAY_TYPE", "MISSING_DATA"]
+chunk_size = 100000
+dist = 0
+print_header = True
+write_mode = 'w'
+coordinates = []
 
-chunksize = 100000;
-dist = 0;
-print_header = True;
-write_mode = 'w';
+for df in pd.read_csv(file, chunksize=chunk_size, iterator=True, dtype={'POLYLINE': str}):
+    # Remove undesired columns from data set
+    print("Start drop columns")
+    df = drop_columns(df, columns_to_remove)
 
-coordinates = [];
+    # Change coordinates format to be python compliant
+    print("Start polyline refactoring")
+    df['POLYLINE'] = df['POLYLINE'].\
+        apply(lambda x: polyline_to_coordinates(x, r'\[(-?[0-9]{1,2}\.[0-9]+),\s(-?[0-9]{1,2}\.[0-9]+)\]'))
 
-for df in pd.read_csv(file, chunksize=chunksize, iterator=True, dtype={'POLYLINE': str}):
-    #Remove undesired colums from dataset
-    print("Start drop colums");
-    df = drop_columns(df, columns_to_remove);
+    print("Start calculating coordinates list")
+    coordinates = itertools.chain(coordinates, list(itertools.chain.from_iterable(df['POLYLINE'])))
 
-    #Change coordinates format to be python compliant
-    print("Start polyline refactoring");
-    df['POLYLINE'] = df['POLYLINE'].apply(lambda x: polyline_to_coordinates(x));
-
-    print("Start calculating coordinates list");
-    coordinates = itertools.chain(coordinates, list(itertools.chain.from_iterable(df['POLYLINE'])));
-
-    #TODO: Esegui rimozione delle tuple senza coordinate
-    #df = df[df['POLYLINE'].size > 0];
+    # TODO: Esegui rimozione delle tuple senza coordinate
+    # df = df[df['POLYLINE'].size > 0]
 
     # Write chunk to result csv file
-    print("Start file writing");
-    df.to_csv(result_csv, mode=write_mode, header=print_header, index=False);
+    print("Start file writing")
+    df.to_csv(result_csv, mode=write_mode, header=print_header, index=False)
 
-    #Print CSV header only the first time
-    print_header = False;
+    # rint CSV header only the first time
+    print_header = False
 
-    #Change write mode to append. In this way, if the file already exists, it is rewrited only the first time.
-    write_mode = 'a';
+    # Change write mode to append. In this way, if the file already exists, it is rewritten only the first time.
+    write_mode = 'a'
 
-#With itertool, coordinates needs to be stored in memory after the execution.
-print("Start coordinates file creation process.");
-coordinates = list(coordinates);
-coordinates_df = pd.DataFrame.from_records(coordinates);
-coordinates_df.to_csv(result_coordinates, mode='w', header=False, index=False);
+# With itertool, coordinates needs to be stored in memory after the execution.
+print("Start coordinates file creation process.")
+coordinates = list(coordinates)
+coordinates_df = pd.DataFrame.from_records(coordinates)
+coordinates_df.to_csv(result_coordinates, mode='w', header=False, index=False)
 
-dist = common.calculate_distance_array(coordinates)/len(coordinates);
-print("Dataset average distance: ", dist);
+dist = common.calculate_distance_array(coordinates)/len(coordinates)
+print("Data set average distance: ", dist)
 
-
-
-print("Start clustering process");
-clusterResult = clustering.make_clustering_2(coordinates, 500);
-
+print("Start clustering process")
+clusterResult = clustering.make_clustering_2(coordinates, 500)
 print("Cluster completed")
-#TODO: non funziona, capire come mai
-#print(clusterResult);
-#print("Saving cluster to file");
-#common.to_json_file(result_folder+'cluster.json', clusterResult.__repr__());
 
+print("Dump model")
+joblib.dump(clusterResult.model, result_model)
+
+print_header = True
+write_mode = 'w'
+for df in pd.read_csv(result_csv, chunksize=chunk_size, iterator=True, dtype={'POLYLINE': str}):
+
+    df['POLYLINE'] = df['POLYLINE'].\
+        apply(lambda x: [clustering.predict(clusterResult.model, point) for point in
+                         polyline_to_coordinates(x, r'\((-?[0-9]{1,2}\.[0-9]+),\s(-?[0-9]{1,2}\.[0-9]+)\)')])
+    df.to_csv(result_csv, mode=write_mode, header=print_header, index=False)
+
+    # Print CSV header only the first time
+    print_header = False
+
+    # Change write mode to append. In this way, if the file already exists, it is rewrited only the first time.
+    write_mode = 'a'
 
 
 
