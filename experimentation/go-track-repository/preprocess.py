23d5ee5a2aabbfc41499f5af0b22bc9dca024a4d
Gianluca Colaianni <fast_luca@hotmail.it>
2018-09-17 09:36:25 +0200

    Go-track-repository preprocess completed

--- experimentation/go-track-repository/preprocess.py
+++ experimentation/go-track-repository/preprocess.py
@@ -7,6 +7,7 @@ result_folder = 'result/';
 file = 'dataset/go_track_trackspoints.csv';
 result_csv = result_folder + 'go_track_trackpoints_cleaned.csv';
 result_coordinates = result_folder + 'coordinates.csv';
+result_model = result_folder + 'model.pkl'
 
 chunksize = 100000;
 dist = 0;
@@ -34,7 +35,37 @@ dist = common.calculate_distance_array(coordinates)/len(coordinates);
 print("Dataset lenght: ", len(coordinates))
 print("Dataset average distance: ", dist);
 
-print("Start clustering process");
-#clusterResult = clustering.make_clustering_2(coordinates, 500);
+print("Start clustering process")
+clusterResult = clustering.make_clustering_2(coordinates, 500)
+print("Cluster completed")
 
+
+print("Start Dump model")
+clustering.dump_model(clusterResult.model, result_model)
+print("End Dump model")
+
+print_header = True
+write_mode = 'w'
+
+gates = [];
+for df in pd.read_csv(result_csv, chunksize=chunksize, iterator=True, dtype={'latitude': float, 'longitude': float}):
+
+    df = pd.DataFrame(df)
+
+    for latitude, longitude in zip(df['latitude'], df['longitude']):
+        gates.append(clustering.predict(clusterResult.model, [latitude, longitude]))
+
+    df['gate'] = gates
+
+    df = df.drop(['latitude', 'longitude'], axis=1)
+
+    df = df.drop_duplicates(['track_id','gate'])
+
+    df.to_csv(result_csv, mode=write_mode, header=print_header, index=False)
+
+    # Print CSV header only the first time
+    print_header = False
+
+    # Change write mode to append. In this way, if the file already exists, it is rewrited only the first time.
+    write_mode = 'a'
 
