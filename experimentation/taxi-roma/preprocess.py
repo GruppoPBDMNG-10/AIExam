4ec837bbd5fd33cebebe09f72884350d49568dd3
Gianluca Colaianni <fast_luca@hotmail.it>
2018-09-22 11:27:56 +0200

    Taxi-roma preprocess

---
+++ experimentation/taxi-roma/preprocess.py
@@ -0,0 +1,114 @@
+from pathlib import Path
+import re
+import pandas as pd
+import numpy as np
+import itertools
+import experimentation.clustering.clustering as clustering
+import experimentation.common.common as common
+
+result_folder = 'result/'
+file = 'dataset/taxi.zip'
+result_csv = result_folder + 'taxi_cleaned.csv'
+result_coordinates = result_folder + 'coordinates.csv'
+result_model = result_folder + 'model.pkl'
+columns_to_remove = ["CALL_TYPE", "ORIGIN_CALL", "ORIGIN_STAND", "DAY_TYPE", "MISSING_DATA"]
+chunk_size = 100000
+dist = 0
+print_header = True
+write_mode = 'w'
+coordinates = []
+
+resultFile = Path(result_csv)
+coordinatesFile = Path(result_coordinates)
+
+if resultFile.is_file() & resultFile.exists() & coordinatesFile.is_file() & coordinatesFile.exists():
+    print("Loading coordinates from file")
+
+    for df in pd.read_csv(coordinatesFile, chunksize=chunk_size, iterator=True, header=None):
+        df = pd.DataFrame(df)
+        coordinates = itertools.chain(coordinates, zip(df.iloc[:, 0], df.iloc[:, 1]))
+
+    print("Start coordinates list creation")
+    coordinates = list(coordinates)
+    print("Coordinates list created")
+
+else:
+    print("Start loading csv file")
+    for df in pd.read_csv(file, chunksize=chunk_size, iterator=True, names=["TAXI_ID", "TIMESTAMP", "POINT"], sep=";",
+                          parse_dates=['time'], infer_datetime_format=True):
+        # Remove undesired columns from data set
+        df = pd.DataFrame(df)
+
+        # Convert datetime to unix timestamp
+        df['time'] = df['time'].apply(lambda x: common.datetime_to_unix(x))
+
+        # Change coordinates format to be python compliant
+        print("Start point refactoring")
+        df['POINT'] = df['POINT']. \
+            apply(
+            lambda x: common.polyline_to_coordinates(x, r'POINT\((-?[0-9]{1,2}\.[0-9]+) (-?[0-9]{1,2}\.[0-9]+)\)'))
+
+        print("Start calculating coordinates list")
+        coordinates = itertools.chain(coordinates, list(itertools.chain.from_iterable(df['POINT'])))
+
+        # Write chunk to result csv file
+        print("Start file writing")
+        df.to_csv(result_csv, mode=write_mode, header=print_header, index=False)
+
+        # rint CSV header only the first time
+        print_header = False
+
+        # Change write mode to append. In this way, if the file already exists, it is rewritten only the first time.
+        write_mode = 'a'
+
+    # With itertool, coordinates needs to be stored in memory after the execution.
+    print("Start coordinates file creation process.")
+    coordinates = list(coordinates)
+    coordinates_df = pd.DataFrame.from_records(coordinates)
+    coordinates_df.to_csv(result_coordinates, mode='w', header=False, index=False)
+    del coordinates_df
+
+dist = common.calculate_average_distance(coordinates)
+print("Data set average distance: ", dist)
+
+clusterFile = Path(result_model)
+clusterResult = None
+clusterModel = None
+
+if clusterFile.is_file() & clusterFile.exists():
+    print("Loading cluster model from file")
+    clusterModel = clustering.model_from_dump(clusterFile)
+    clusterResult = clustering.Result(0, 0, 0, clusterModel)
+    print("Cluster model loaded: ", clusterModel)
+else:
+    print("Start clustering process")
+    clusterResult = clustering.make_clustering_2(coordinates, 500)
+    print("Cluster completed")
+    print("Dump model")
+    clustering.dump_model(clusterResult.model, result_model)
+    print("Dump completed")
+
+del coordinates
+
+print_header = True
+write_mode = 'w'
+
+for df in pd.read_csv(result_csv, chunksize=chunk_size, iterator=True, dtype={'POLYLINE': str}):
+    df = pd.DataFrame(df)
+
+    df['POINT'] = df['POINT'].apply(lambda x: [clustering.predict(clusterResult.model, point) for point in
+                                               common.polyline_to_coordinates(x,
+                                                                              r'\((-?[0-9]{1,2}\.[0-9]+),\s(-?[0-9]{1,2}\.[0-9]+)\)')]).astype(
+        str)
+
+    df.rename(columns={df.columns[3]: "GATE"}, inplace=True)
+
+    df.drop(['TAXI_ID', 'GATE'], inplace=True)
+
+    df.to_csv(result_folder + 'taxi_rome_gate.csv', mode=write_mode, header=print_header, index=False)
+
+    # Print CSV header only the first time
+    print_header = False
+
+    # Change write mode to append. In this way, if the file already exists, it is rewrited only the first time.
+    write_mode = 'a'
