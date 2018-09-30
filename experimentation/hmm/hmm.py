bf5ad8432c36528bf4f80a7b9d1f134a7e21343e
Gianluca Colaianni <fast_luca@hotmail.it>
2018-09-30 20:03:29 +0200

    Fixes

--- experimentation/hmm/hmm.py
+++ experimentation/hmm/hmm.py
@@ -2,6 +2,7 @@ import numpy as np
 from hmmlearn import hmm
 import pandas as pd
 import itertools
+from sklearn.externals import joblib
 
 
 def __process_raw(df, sequence_map=dict):
@@ -16,48 +17,60 @@ def __load_df(file) -> dict:
     chunk_size = 500000
     print("Start loading file")
     sequence_map = {}
-    for df in pd.read_csv(file, chunksize=chunk_size, iterator=True, dtype={'TRIP_ID': str, 'DRIVER_ID': int, 'TIMESTAMP': int, 'GATE': int}):
+    for df in pd.read_csv(file, chunksize=chunk_size, iterator=True,
+                          dtype={'TRIP_ID': str, 'DRIVER_ID': int, 'TIMESTAMP': int, 'GATE': int}):
         df.apply(lambda x: __process_raw(x, sequence_map), axis=1)
 
     return sequence_map
 
 
-def build_model(file) -> hmm.MultinomialHMM:
+def prepare_dataset_rapresentation(file) -> (dict, list):
+    """Load from the specified file the dataset. The result is a dict having as key the TRIP_ID and as values the
+    one-hot representation of each sequence."""
+
     sequences_map = __load_df(file)
     gates = sorted(list(set(itertools.chain.from_iterable(sequences_map.values()))))
     gates_index = dict((gate, gates.index(gate)) for gate in gates)
 
-    max_len = len(max(sequences_map.values(), key=len))
+    result = dict()
 
-    print("Max sequence length:", max_len)
+    for key, sequence in sequences_map.items():
+        for gate in sequence:
+            elem = np.zeros((len(gates), 1), dtype=np.int8)
+            elem[gates_index[gate]] = 1
+            result[key] = elem
 
-    vector = np.zeros((len(sequences_map.values()), max_len, len(gates)), dtype=np.short)
-
-    for i, sequence in enumerate(sequences_map.values()):
-        for t, gate in enumerate(sequence):
-            vector[i, t, gates_index[gate]] = 1
-
-    n_samples, nx, ny = vector.shape
-    d2_train_dataset = vector.reshape((n_samples * nx * ny), 1)
-
-    lengths = [max_len*len(gates) for i in range(len(sequences_map.values()))]
-
-    model_to_use = None
-    component_to_use = 2
-    best_score = None
-    for i in range(component_to_use, len(gates) + 1):
-        print("Start training")
-        model = hmm.MultinomialHMM(n_components=i, random_state=42, n_iter=1000, tol=0.001)
-        model = model.fit(d2_train_dataset, lengths=lengths)
-        score = model.score(d2_train_dataset, lengths=lengths)
-        if model_to_use is None or score > best_score:
-            model_to_use = model
-            best_score = score
-            component_to_use = i
-        print("States:", i, "Score:", score)
-
-    print("Model to use:", model_to_use)
-    return model_to_use
+    return result, gates
 
 
-build_model('../taxi-roma/result/taxi_rome_gate.csv')
+def build_model(dataset=dict) -> hmm.MultinomialHMM:
+    """It builds a MultinomialHMM from the dataset. The input dataset expected has the same form obtained from
+    prepare_dataset_rapresentation method."""
+
+    vector = []
+    lengths = []
+
+    for sequence in dataset.values():
+        vector.append(sequence)
+        lengths.append(len(sequence))
+
+    vector = np.concatenate(vector)
+
+    print("Start model training")
+    model = hmm.MultinomialHMM(n_components=2, random_state=42, n_iter=1000, tol=0.001)
+    model = model.fit(vector, lengths=lengths)
+
+    return model
+
+
+def dump(model, filename):
+    joblib.dump(model, filename)
+
+
+def load_dump(filename):
+    return joblib.load(filename, hmm.MultinomialHMM)
+
+
+dataset, gates = prepare_dataset_rapresentation('../taxi-roma/result/taxi_rome_gate.csv')
+model = build_model(dataset)
+print("Model created:", model)
