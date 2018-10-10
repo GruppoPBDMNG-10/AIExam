a50f64aa0a773c474b19a353213b180cde8376bb
Gianluca Colaianni <fast_luca@hotmail.it>
2018-10-10 16:33:12 +0200

    HMM taxi-porto

--- experimentation/taxi-porto/experimentation_hmm.py
+++ experimentation/taxi-porto/experimentation_hmm.py
@@ -11,31 +11,12 @@ DATASET_PATH = RESULT_PATH + 'taxi_porto_gate.csv'
 MODE = ''
 MAX_TEST_LENGTH = 7000
 
-
-def calcualate_scores_dict(dataset=dict, model=hmm.hmm.MultinomialHMM) -> dict:
-    result = dict((key, model.score(value.toarray(), [len(value.toarray())]) / len(value.toarray())) for key, value in dataset.items())
-    return result
-
-
-def retrieve_test_samples(dataset=dict, min_sequence_length=3, max_sequence_length=-1, max_test_data_length=-1) -> dict:
-    print("Dataset samples before filtering:", len(dataset.keys()))
-    if max_sequence_length > 0:
-        result = dict(
-            (key, value) for key, value in dataset.items() if max_sequence_length >= len(value.toarray()) >= min_sequence_length)
-    else:
-        result = dict((key, value) for key, value in dataset.items() if len(value.toarray()) >= min_sequence_length)
-    if max_test_data_length > 0 and len(result.keys()) > max_test_data_length:
-        result = dict(itertools.islice(result.items(), max_test_data_length))
-    print("Dataset samples after filtering:", len(result.keys()))
-    return result
-
-
 dataset, gates = hmm.prepare_dataset_rapresentation(DATASET_PATH)
 hmm_model_file = Path(MODEL_PATH)
 
 print("Dataset loading completed")
 
-test_data = retrieve_test_samples(dataset, min_sequence_length=5, max_sequence_length=150,
+test_data = hmm.retrieve_test_samples(dataset, min_sequence_length=5, max_sequence_length=150,
                                   max_test_data_length=MAX_TEST_LENGTH)
 
 model = None
@@ -50,7 +31,7 @@ else:
     hmm.dump(model, MODEL_PATH)
 
 print("Start calculating score for each sample")
-scores_dict = calcualate_scores_dict(dataset, model)
+scores_dict = hmm.calcualate_scores_dict(dataset, model)
 
 print('Saving scores dictionary')
 
